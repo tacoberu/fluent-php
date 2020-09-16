efc72fbf3e9d98448f02054e2b1efaa5cde2d4ee
Martin Takáč <martin@takac.name>
2020-09-17 01:40:26 +0200

    fix tests of NumberIntl

--- tests/Intl/NumberIntlTest.php
+++ tests/Intl/NumberIntlTest.php
@@ -16,9 +16,9 @@ class NumberIntlTest extends PHPUnit_Framework_TestCase
 	/**
 	 * @dataProvider dataCorrect
 	 */
-	function testCorrect($val, $opts, $expected)
+	function testCorrect($locale, $val, $opts, $expected)
 	{
-		$this->assertSame($expected, NumberIntl::format($val, $opts));
+		$this->assertSame($expected, $this->getFormater($locale)->format($val, $opts));
 	}
 
 
@@ -26,22 +26,30 @@ class NumberIntlTest extends PHPUnit_Framework_TestCase
 	function dataCorrect()
 	{
 		return [
-			[3.14, [], '3.14'],
-			[3, [], '3'],
+			['cs-CZ', 3.14, [], '3.14'],
+			['cs-CZ', 3, [], '3'],
 
-			[3.14, ['minimumFractionDigits' => 0,], '3.14'],
-			[3.14, ['minimumFractionDigits' => 2,], '3.14'],
-			[3.14, ['minimumFractionDigits' => 4,], '3.1400'],
+			['cs-CZ', 3.14, ['minimumFractionDigits' => 0,], '3.14'],
+			['cs-CZ', 3.14, ['minimumFractionDigits' => 2,], '3.14'],
+			['cs-CZ', 3.14, ['minimumFractionDigits' => 4,], '3.1400'],
 
-			[3, ['minimumFractionDigits' => 2,], '3.00'],
+			['cs-CZ', 3, ['minimumFractionDigits' => 2,], '3.00'],
 
-			[3.14, ['maximumFractionDigits' => 0,], '3.14'],
-			[3.14, ['maximumFractionDigits' => 2,], '3.14'],
-			[3.14, ['maximumFractionDigits' => 4,], '3.14'],
-			[3.14, ['maximumFractionDigits' => 4,'minimumFractionDigits' => 3,], '3.140'],
-			[3, ['maximumFractionDigits' => 2,], '3'],
+			['cs-CZ', 3.14, ['maximumFractionDigits' => 0,], '3.14'],
+			['cs-CZ', 3.14, ['maximumFractionDigits' => 2,], '3.14'],
+			['cs-CZ', 3.14, ['maximumFractionDigits' => 4,], '3.14'],
+			['cs-CZ', 3.14, ['maximumFractionDigits' => 4,'minimumFractionDigits' => 3,], '3.140'],
+			['cs-CZ', 3, ['maximumFractionDigits' => 2,], '3'],
 
 		];
+	}
+
+
+
+
+	private function getFormater($locale = 'cs-CZ')
+	{
+		return new NumberIntl($locale);
 	}
 
 }
